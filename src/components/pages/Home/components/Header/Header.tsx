ca259a18cdcee3a9dde812ab1b4281e52ecb3fad
limichange <limichange@hotmail.com>
2022-05-16 10:10:32 +0800

    fix: image load twice

--- src/components/pages/Home/components/Header/Header.tsx
+++ src/components/pages/Home/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import { motion } from 'framer-motion';
 
 import { usePositionAnimation } from '../../../../../hooks';
-import { mergeRefs } from '../../../../../utils';
+import { isBrowser, mergeRefs } from '../../../../../utils';
 import { Box } from '../../../../base';
 
 import { Background } from './Background';
@@ -50,7 +50,7 @@ export const Header: FC = () => {
             paddingBottom: 78,
           }}
         >
-          <Background />
+          {isBrowser() && <Background />}
 
           <Content />
         </Box>
