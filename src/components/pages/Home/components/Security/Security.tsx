ae60c958cb51a4bde41dd27c5022261b2474ac72
limichange <limichange@hotmail.com>
2022-05-18 22:32:04 +0800

    feat: SecuritySwiper component

--- src/components/pages/Home/components/Security/Security.tsx
+++ src/components/pages/Home/components/Security/Security.tsx
@@ -3,94 +3,130 @@ import { FC, useState } from 'react';
 import { useTheme } from '@emotion/react';
 import { AnimatePresence, motion } from 'framer-motion';
 
+import { useMediaQuery } from '../../../../../hooks';
 import { Box, Container, Flex, Section } from '../../../../base';
 
 import { Item } from './Item';
 import { SecurityHeader } from './SecurityHeader';
+import { SecuritySwiper } from './SecuritySwiper';
 import { SecurityDataItem, useSecurityData } from './useSecurityData';
 
 export const Security: FC = () => {
   const theme = useTheme();
   const data = useSecurityData();
+  const media = useMediaQuery();
   const [currentItem, setCurrentItem] = useState<SecurityDataItem>(data[0]);
 
   return (
     <Section>
-      <Container>
-        <Flex
+      {/* pc */}
+      {media.medium && (
+        <Container
           xs={{
-            flexDirection: 'column',
+            display: 'none',
+          }}
+          l={{
+            display: 'block',
+          }}
+        >
+          <Flex
+            xs={{
+              flexDirection: 'column',
+              paddingTop: 16,
+              paddingBottom: 16,
+              backgroundColor: theme.colors.white,
+              gap: 46,
+              label: 'Security',
+            }}
+            m={{
+              flexDirection: 'row',
+            }}
+          >
+            {/* left */}
+            <Flex
+              xs={{
+                flex: 1,
+                flexDirection: 'column',
+                gap: 68,
+              }}
+            >
+              <SecurityHeader />
+
+              <Flex
+                xs={{
+                  flexDirection: 'column',
+                  gap: 32,
+                }}
+              >
+                {data.map((item) => (
+                  <Item
+                    key={item.title}
+                    onPointerEnter={() => {
+                      setCurrentItem(item);
+                    }}
+                    title={item.title}
+                    description={item.description}
+                  />
+                ))}
+              </Flex>
+            </Flex>
+
+            {/* right */}
+            <Box
+              xs={{
+                background: theme.background.test100,
+                flex: 1,
+                borderRadius: 40,
+                overflow: 'hidden',
+                height: 'fit-content',
+              }}
+            >
+              <AnimatePresence exitBeforeEnter>
+                <motion.div
+                  key={currentItem.title}
+                  animate={{ opacity: 1 }}
+                  initial={{ opacity: 0 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.1 }}
+                >
+                  {currentItem?.image}
+                </motion.div>
+              </AnimatePresence>
+            </Box>
+          </Flex>
+        </Container>
+      )}
+
+      {!media.medium && (
+        <Box
+          xs={{
             paddingTop: 16,
             paddingBottom: 16,
             backgroundColor: theme.colors.white,
-            gap: 46,
-            label: 'Security',
-          }}
-          m={{
-            flexDirection: 'row',
           }}
         >
-          {/* left */}
-          <Flex
+          <Box
             xs={{
-              flex: 1,
-              flexDirection: 'column',
-              gap: 68,
+              paddingLeft: 24,
             }}
           >
             <SecurityHeader />
+          </Box>
 
-            {/* hide it, when mobile version */}
-            <Box
-              xs={{
-                display: 'none',
-                flexDirection: 'column',
-                gap: 32,
-              }}
-              l={{
-                display: 'flex',
-              }}
-            >
-              {data.map((item) => (
-                <Item
-                  key={item.title}
-                  onPointerEnter={() => {
-                    setCurrentItem(item);
-                  }}
-                  {...item}
-                />
-              ))}
-            </Box>
-          </Flex>
-
-          {/* right, hide it when mobile viewpoint */}
           <Box
             xs={{
-              display: 'none',
-              background: theme.background.test100,
-              flex: 1,
-              borderRadius: 40,
+              marginTop: 46,
+              width: '100vw',
               overflow: 'hidden',
-              height: 'fit-content',
             }}
             l={{
-              display: 'block',
+              display: 'none',
             }}
           >
-            <AnimatePresence exitBeforeEnter>
-              <motion.div
-                key={currentItem.title}
-                animate={{ opacity: 1 }}
-                initial={{ opacity: 0 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.1 }}
-              >
-                {currentItem?.image}
-              </motion.div>
-            </AnimatePresence>
+            <SecuritySwiper />
           </Box>
-        </Flex>
-      </Container>
+        </Box>
+      )}
     </Section>
   );
 };
---
+++ src/components/pages/Home/components/Security/SecuritySwiper/SecuritySwiper.tsx
@@ -0,0 +1,50 @@
+import { FC, ReactNode } from 'react';
+
+import { FreeMode } from 'swiper';
+
+import { Swiper as SwiperComponent, SwiperSlide } from '../../../../../base';
+import { useSecurityData } from '../useSecurityData';
+
+import { SecuritySwiperItem } from './SecuritySwiperItem';
+
+export interface SecuritySwiperProps {
+  children?: ReactNode;
+}
+
+export const SecuritySwiper: FC<SecuritySwiperProps> = (props) => {
+  const { children } = props;
+  const data = useSecurityData();
+
+  return (
+    <SwiperComponent
+      slidesPerView="auto"
+      spaceBetween={30}
+      modules={[FreeMode]}
+      freeMode={{
+        enabled: true,
+      }}
+      style={{
+        paddingLeft: 24,
+        paddingRight: 24,
+      }}
+    >
+      {data.map((item) => (
+        <SwiperSlide
+          style={{
+            minWidth: 272,
+            width: '30vw',
+          }}
+        >
+          <SecuritySwiperItem
+            key={item.title}
+            imageNode={item.image}
+            title={item.title}
+            description={item.description}
+          />
+        </SwiperSlide>
+      ))}
+
+      {children}
+    </SwiperComponent>
+  );
+};
