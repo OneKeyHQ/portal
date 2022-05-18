b7decd085b26a13353cf3a58ba8c428b1c0e04f3
limichange <limichange@hotmail.com>
2022-05-18 16:13:33 +0800

    feat: clean Security section tag type

--- src/components/pages/Home/components/Security/Security.tsx
+++ src/components/pages/Home/components/Security/Security.tsx
@@ -2,7 +2,15 @@ import { FC } from 'react';
 
 import { useTheme } from '@emotion/react';
 
-import { Box, Button, Container, Flex } from '../../../../base';
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Img,
+  Section,
+  Span,
+} from '../../../../base';
 
 import placeholderImage from './images/placeholder.png';
 import { Item } from './Item';
@@ -13,9 +21,9 @@ export const Security: FC = () => {
   const data = useData();
 
   return (
-    <section>
+    <Section>
       <Container>
-        <div
+        <Box
           css={{
             paddingTop: 16,
             paddingBottom: 16,
@@ -25,33 +33,36 @@ export const Security: FC = () => {
             label: 'Security',
           }}
         >
-          <Flex direction="column" gap={68}>
-            <div
+          <Flex
+            xs={{
+              flexDirection: 'column',
+              gap: 68,
+            }}
+          >
+            <Flex
               css={{
-                display: 'flex',
                 flexDirection: 'column',
                 gap: 24,
                 flexGrow: 1,
               }}
             >
-              <Box css={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
-                <span css={{ ...theme.text.medium700, color: '#8a9195' }}>
+              <Flex css={{ flexDirection: 'column', gap: 8 }}>
+                <Span css={{ ...theme.text.medium700, color: '#8a9195' }}>
                   SECURITY
-                </span>
-                <span css={{ ...theme.text.medium1100, color: '#101111' }}>
+                </Span>
+                <Span css={{ ...theme.text.medium1100, color: '#101111' }}>
                   Security,
                   <br /> both hard and soft.
-                </span>
-              </Box>
+                </Span>
+              </Flex>
 
               <Box>
                 <Button variant="outline">Learn More</Button>
               </Box>
-            </div>
+            </Flex>
 
-            <div
+            <Flex
               css={{
-                display: 'flex',
                 flexDirection: 'column',
                 gap: 32,
                 flexGrow: 1,
@@ -60,19 +71,19 @@ export const Security: FC = () => {
               {data.map((item) => (
                 <Item {...item} />
               ))}
-            </div>
+            </Flex>
           </Flex>
 
           {/* right */}
           <Box css={{ borderRadius: 40, overflow: 'hidden' }}>
-            <img
+            <Img
               alt="sideImage.png"
               src={placeholderImage}
               css={{ width: 633 }}
             />
           </Box>
-        </div>
+        </Box>
       </Container>
-    </section>
+    </Section>
   );
 };
