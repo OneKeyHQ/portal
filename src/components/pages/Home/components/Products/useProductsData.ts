cf496824893e0414824510f7cde4516d37f2edce
limichange <limichange@hotmail.com>
2022-05-19 10:28:20 +0800

    feat: create useProductsData and import it

---
+++ src/components/pages/Home/components/Products/useProductsData.ts
@@ -0,0 +1,14 @@
+export function useProductsData() {
+  return [
+    {
+      title: 'Advanced on-chain wallet app',
+      description:
+        "Use OneKey App separately to store and use your crypto assets in a secure and convenient manner. Each user's assets are appropriately secured and stored locally.",
+    },
+    {
+      title: 'Need more security? No problem.',
+      description:
+        "Hardware wallets take the securitisation of digital assets to another level. From physically isolating hackers to preventing Trojan horses and software attacks. All transfers must be approved by the hardware wallet, so you don't have to worry about the loss or theft of your mobile phone.",
+    },
+  ];
+}
