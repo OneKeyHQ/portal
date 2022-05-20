b35adbabcb7af6709b39443b0874f7f8e35df7a0
limichange <limichange@hotmail.com>
2022-05-20 16:14:19 +0800

    feat: add Hardware coming-soon status style

--- src/components/pages/Home/components/Hardware/Item.tsx
+++ src/components/pages/Home/components/Hardware/Item.tsx
@@ -5,24 +5,29 @@ import { useTheme } from '@emotion/react';
 import { useHover } from '../../../../../hooks';
 import { Box, Span } from '../../../../base';
 
+import { ComingSoon } from './ComingSoon';
+
 export interface ItemProps {
   image: string;
   title: string;
   description: string;
   hoverImage?: string;
+  status?: string;
 }
 
 export const Item: FC<ItemProps> = (props) => {
-  const { image, title, description, hoverImage } = props;
+  const { image, title, description, hoverImage, status } = props;
   const theme = useTheme();
   const { hoverProps, isHovered } = useHover({
     timeout: 100,
+    isDisabled: status === 'coming-soon',
   });
 
   return (
     <Box
       {...hoverProps}
       css={{
+        opacity: status === 'coming-soon' ? 0.5 : 1,
         display: 'flex',
         flexDirection: 'column',
         gap: 24,
@@ -55,15 +60,27 @@ export const Item: FC<ItemProps> = (props) => {
           }}
         />
         <Box css={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
-          <Span
+          <Box
             xs={{
+              display: 'flex',
+              alignItems: 'center',
               ...theme.text.medium500,
               color: theme.colors.white,
             }}
             m={{ ...theme.text.medium600 }}
           >
             {title}
-          </Span>
+
+            {status === 'coming-soon' && (
+              <Span
+                xs={{
+                  marginLeft: 8,
+                }}
+              >
+                <ComingSoon />
+              </Span>
+            )}
+          </Box>
           <Span
             xs={{
               ...theme.text.normal200,
