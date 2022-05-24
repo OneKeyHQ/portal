ed076321be7de1b5567150d7501b62307621a795
limichange <limichange@hotmail.com>
2022-05-24 15:59:20 +0800

    feat: add RightArea link and hover style

--- src/components/base/Navigation/NormalNavigation/ProductPanel/ProductPanel.tsx
+++ src/components/base/Navigation/NormalNavigation/ProductPanel/ProductPanel.tsx
@@ -37,6 +37,8 @@ export const ProductPanel: FC<ProductPanelProps> = (props) => {
   const { children, isActive } = props;
   const { hoverProps, isHovered } = useHover({ timeout: 100 });
   const [cursorVariant, setCursorVariant] = useState('hidden');
+  const { hoverProps: RightAreaHoverProps, isHovered: isRightAreaHovered } =
+    useHover({ timeout: 100 });
 
   useEffect(() => {
     if (isHovered || isActive) {
@@ -54,35 +56,41 @@ export const ProductPanel: FC<ProductPanelProps> = (props) => {
       style={{
         width: '100%',
         overflow: 'hidden',
-        background:
-          'linear-gradient(90deg, #F0F1F2 0%, #F0F1F2 53.76%, rgba(255, 255, 255, 1) 56.6%)',
       }}
     >
-      <Container>
-        <Box
-          {...hoverProps}
-          xs={{
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
+      <Box
+        {...hoverProps}
+        xs={{
+          background: `linear-gradient(90deg, #F0F1F2 0%, #F0F1F2 50%, ${
+            isRightAreaHovered ? '#F8F8F8' : '#FFFFFF'
+          } 50%)`,
+        }}
+      >
+        <Container>
           <Box
             xs={{
-              flex: 1,
+              display: 'flex',
+              justifyContent: 'space-between',
             }}
           >
-            <LeftArea />
+            <Box
+              xs={{
+                flex: 1,
+              }}
+            >
+              <LeftArea />
+            </Box>
+            <Box
+              xs={{
+                width: '33%',
+              }}
+            >
+              <RightArea {...RightAreaHoverProps} />
+            </Box>
           </Box>
-          <Box
-            xs={{
-              width: '33%',
-            }}
-          >
-            <RightArea />
-          </Box>
-        </Box>
-        {children}
-      </Container>
+          {children}
+        </Container>
+      </Box>
     </motion.div>
   );
 };
