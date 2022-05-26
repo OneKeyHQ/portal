6338aee47fec393375e7e4b62cdcea1868bc362e
limichange <limichange@hotmail.com>
2022-05-26 12:59:13 +0800

    feat: footer link title first letter uppercase

--- src/components/base/PageFooter/MenuList.tsx
+++ src/components/base/PageFooter/MenuList.tsx
@@ -45,6 +45,9 @@ export const MenuList: FC = () => {
               ...theme.text.medium200,
               color: theme.colors.white,
               opacity: 0.6,
+              ':first-letter': {
+                textTransform: 'uppercase',
+              },
             }}
           >
             {item.name}
