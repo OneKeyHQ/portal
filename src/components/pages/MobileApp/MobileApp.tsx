8903699a50a65579cdedde683d4018188cee85dd
limichange <limichange@hotmail.com>
2022-05-31 16:44:49 +0800

    feat: test link

--- src/components/pages/MobileApp/MobileApp.tsx
+++ src/components/pages/MobileApp/MobileApp.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect } from 'react';
 
 import { detect } from 'detect-browser';
 import { navigate } from 'gatsby';
@@ -12,19 +12,31 @@ export interface MobileAppProps {
 export const MobileApp: FC<MobileAppProps> = (props) => {
   const { children } = props;
 
-  if (isBrowser()) {
-    const browser = detect();
+  useEffect(() => {
+    if (isBrowser()) {
+      const browser = detect();
 
-    if (browser?.os === 'iOS') {
-      navigate('https://apps.apple.com/app/id1609559473');
-    } else if (browser?.os === 'Android OS') {
-      navigate(
-        'https://play.google.com/store/apps/details?id=com.bixin.wallet.mainnet',
-      );
-    } else {
-      navigate('https://onekey.so/download');
+      if (browser?.os === 'iOS') {
+        navigate('https://itunes.apple.com/app/chrome/id1609559473');
+      } else if (browser?.os === 'Android OS') {
+        navigate(
+          'https://play.google.com/store/apps/details?id=com.bixin.wallet.mainnet',
+        );
+      } else {
+        navigate('https://onekey.so/download');
+      }
     }
-  }
 
-  return <div>{children}</div>;
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+      {children}
+      {detect()?.os}
+      <a href="https://itunes.apple.com/app/chrome/id1609559473">
+        https://itunes.apple.com/app/chrome/id1609559473
+      </a>
+    </div>
+  );
 };
