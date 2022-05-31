ba078fbb5516ed0284904244d5a9b4bebd58a005
limichange <limichange@hotmail.com>
2022-05-31 09:45:21 +0800

    feat: setting background

---
+++ src/components/pages/App/DownloadPanel/Background/Background.tsx
@@ -0,0 +1,38 @@
+import { FC, ReactNode } from 'react';
+
+import { Box, Img } from '../../../../base';
+
+import pcImage from './images/pc.jpg';
+
+export interface BackgroundProps {
+  children?: ReactNode;
+}
+
+export const Background: FC<BackgroundProps> = (props) => {
+  const { children } = props;
+
+  return (
+    <Box
+      xs={{
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      <Img
+        xs={{
+          marginLeft: 'auto',
+          display: 'block',
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+        }}
+        l={{
+          width: '50%',
+        }}
+        src={pcImage}
+      />
+
+      {children}
+    </Box>
+  );
+};
--- src/components/pages/App/DownloadPanel/DownloadPanel.tsx
+++ src/components/pages/App/DownloadPanel/DownloadPanel.tsx
@@ -2,7 +2,9 @@ import { FC, ReactNode } from 'react';
 
 import { useTheme } from '@emotion/react';
 
-import { Box } from '../../../base';
+import { AppleIcon, Box, Container } from '../../../base';
+
+import { Background } from './Background';
 
 export interface DownloadPanelProps {
   children?: ReactNode;
@@ -15,12 +17,19 @@ export const DownloadPanel: FC<DownloadPanelProps> = (props) => {
   return (
     <Box
       xs={{
+        overflow: 'hidden',
+        width: '100%',
         height: '100vh',
         minHeight: 720,
         maxHeight: 960,
         backgroundColor: theme.colors.white,
       }}
     >
+      <Background />
+
+      <Container>
+        <AppleIcon width={20} />
+      </Container>
       {children}
     </Box>
   );
