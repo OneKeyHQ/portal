69998aa130649f7d08c9918a8ae4c68a30ba759d
limichange <limichange@hotmail.com>
2022-05-31 15:34:58 +0800

    feat: auto jump

--- src/components/pages/MobileApp/MobileApp.tsx
+++ src/components/pages/MobileApp/MobileApp.tsx
@@ -1,4 +1,6 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect } from 'react';
+
+import { detect } from 'detect-browser';
 
 export interface MobileAppProps {
   children?: ReactNode;
@@ -7,5 +9,17 @@ export interface MobileAppProps {
 export const MobileApp: FC<MobileAppProps> = (props) => {
   const { children } = props;
 
-  return <div>MobileApp {children}</div>;
+  useEffect(() => {
+    const browser = detect();
+
+    if (browser?.os === 'iOS') {
+      window.location.href = 'https://onekey.so/download?client=ios';
+    } else if (browser?.os === 'Android OS') {
+      window.location.href = 'https://onekey.so/download?client=android';
+    } else {
+      window.location.href = 'https://onekey.so/download';
+    }
+  }, []);
+
+  return <div>{children}</div>;
 };
