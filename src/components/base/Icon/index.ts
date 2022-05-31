f2bd797b7d0599fa3bcd5203c3773e89cf682ce2
limichange <limichange@hotmail.com>
2022-05-31 09:17:02 +0800

    feat: icon auto generate

--- src/components/base/Icon/index.ts
+++ src/components/base/Icon/index.ts
@@ -3,5 +3,4 @@ export * from './DiscordIcon';
 export * from './GithubIcon';
 export * from './MenuCloseIcon';
 export * from './MenuListArrowIcon';
-export { ReactComponent as MenuIcon } from './MenuIcon.svg';
-export { default as MenuIconInline } from './MenuIcon.svg';
+export * from './IconAutoGenerate';
