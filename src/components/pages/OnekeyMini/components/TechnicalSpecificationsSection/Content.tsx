10a378a4461455503801a4ab187db1d924f91e0f
limichange <limichange@hotmail.com>
2022-06-09 16:23:22 +0800

    feat: adjust Content text responsive style

--- src/components/pages/OnekeyMini/components/TechnicalSpecificationsSection/Content.tsx
+++ src/components/pages/OnekeyMini/components/TechnicalSpecificationsSection/Content.tsx
@@ -1,31 +1,33 @@
-import { FC, ReactNode } from 'react';
+import { FC } from 'react';
 
 import { useTheme } from '@emotion/react';
 
 import { Flex, Span } from '../../../../base';
 
-export interface ContentProps {
-  children?: ReactNode;
-}
-
-export const Content: FC<ContentProps> = (props) => {
-  const { children } = props;
+export const Content: FC = () => {
   const theme = useTheme();
   const { medium300, medium600, normal200 } = theme.text;
+  const m3 = { ...medium300, opacity: 0.5 };
+  const n2 = { ...normal200, opacity: 0.5 };
 
   return (
-    <Flex xs={{ gap: 24, flexGrow: 1, color: theme.colors.white }}>
-      <Flex xs={{ flexDirection: 'column', gap: 64 }}>
+    <Flex
+      xs={{ flexDirection: 'column', color: theme.colors.white, gap: 64 }}
+      m={{ paddingTop: 36 }}
+      l={{ paddingTop: 56 }}
+      xl={{ flexDirection: 'row' }}
+    >
+      <Flex xs={{ flexDirection: 'column', gap: 64, flex: 1 }}>
         <Flex xs={{ flexDirection: 'column', gap: 40 }}>
           <Flex xs={{ flexDirection: 'column', gap: 16 }}>
             <Span xs={medium600}>Dimensions & weight</Span>
             <Flex xs={{ flexDirection: 'column', gap: 4 }}>
-              <Span xs={medium300}>Size</Span>
-              <Span xs={normal200}>72mm x 18.6mm x 11.75mm</Span>
+              <Span xs={m3}>Size</Span>
+              <Span xs={n2}>72mm x 18.6mm x 11.75mm</Span>
             </Flex>
             <Flex xs={{ flexDirection: 'column', gap: 4 }}>
-              <Span xs={medium300}>Weight</Span>
-              <Span xs={normal200}>34g</Span>
+              <Span xs={m3}>Weight</Span>
+              <Span xs={n2}>34g</Span>
             </Flex>
           </Flex>
         </Flex>
@@ -33,29 +35,31 @@ export const Content: FC<ContentProps> = (props) => {
           <Flex xs={{ flexDirection: 'column', gap: 16 }}>
             <Span xs={medium600}>Microcontrollers & connector</Span>
             <Flex xs={{ flexDirection: 'column', gap: 4 }}>
-              <Span xs={medium300}>Connector</Span>
-              <Span xs={normal200}>USB Type-C</Span>
+              <Span xs={m3}>Connector</Span>
+              <Span xs={n2}>USB Type-C</Span>
             </Flex>
             <Flex xs={{ flexDirection: 'column', gap: 4 }}>
-              <Span xs={medium300}>Certification level</Span>
-              <Span xs={normal200}>CC EAL5+</Span>
+              <Span xs={m3}>Certification level</Span>
+              <Span xs={n2}>CC EAL5+</Span>
             </Flex>
             <Flex xs={{ flexDirection: 'column', gap: 4 }}>
-              <Span xs={medium300}>Chips</Span>
-              <Span xs={normal200}>ST33J2M0</Span>
+              <Span xs={m3}>Chips</Span>
+              <Span xs={n2}>ST33J2M0</Span>
             </Flex>
           </Flex>
         </Flex>
         <Flex xs={{ flexDirection: 'column', gap: 40 }}>
           <Flex xs={{ flexDirection: 'column', gap: 16 }}>
             <Span xs={medium600}>Materials</Span>
-            <Span xs={normal200}>Brushed stainless steel and plastic.</Span>
+            <Span xs={n2}>Brushed stainless steel and plastic.</Span>
           </Flex>
         </Flex>
+      </Flex>
+      <Flex xs={{ flexDirection: 'column', gap: 64, flex: 1 }}>
         <Flex xs={{ flexDirection: 'column', gap: 40 }}>
           <Flex xs={{ flexDirection: 'column', gap: 16 }}>
             <Span xs={medium600}>Compatibility</Span>
-            <Span xs={normal200}>
+            <Span xs={n2}>
               64-bits desktop computer (Windows 8.1+, macOS 10.14+, Linux)
               excluding ARM Processors. Please note that our devices and the
               Ledger Live App are not compatible with Chromebooks. Also
@@ -67,15 +71,13 @@ export const Content: FC<ContentProps> = (props) => {
         <Flex xs={{ flexDirection: 'column', gap: 40 }}>
           <Flex xs={{ flexDirection: 'column', gap: 16 }}>
             <Span xs={medium600}>Flex Contents</Span>
-            <Span xs={normal200}>
+            <Span xs={n2}>
               1 hardware wallet, 1 USB-C to USB-A cable, 1 getting started
               leaflet, 3 recovery sheets, 1 keychain strap
             </Span>
           </Flex>
         </Flex>
       </Flex>
-
-      {children}
     </Flex>
   );
 };
