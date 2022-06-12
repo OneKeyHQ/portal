1f1ba15bcd6b94cd0b52e1b39c3e5830f5054d56
limichange <limichange@hotmail.com>
2022-06-12 11:45:50 +0800

    feat: FeatureSection mobile layout style

--- src/components/pages/Security/FeatureSection/FeatureSection.tsx
+++ src/components/pages/Security/FeatureSection/FeatureSection.tsx
@@ -1,7 +1,9 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useState } from 'react';
 
 import { useTheme } from '@emotion/react';
+import Swiper from 'swiper';
 
+import { useBoundingClientRect, useInterval } from '../../../../hooks';
 import { Box, Container, Section, SwiperSlide } from '../../../base';
 import { FreeSwiper } from '../../../common';
 
@@ -18,6 +20,26 @@ export const FeatureSection: FC<FeatureSectionProps> = (props) => {
   const { children } = props;
   const theme = useTheme();
   const featureSectionData = useFeatureSectionData();
+  const [blockSwiper, setBlockSwiper] = useState<Swiper>();
+  const [progress, setProgress] = useState(0);
+  const progressBarWidth = 100;
+  const { ref, rect } = useBoundingClientRect();
+  const progressContainerWidth = rect?.width ?? 0;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const updateProgress = () => {
+    if (blockSwiper?.progress) {
+      setCurrentIndex(
+        Math.abs(parseInt((blockSwiper.progress * 2).toFixed(0))),
+      );
+
+      setProgress(blockSwiper?.progress);
+    }
+  };
+
+  useInterval(() => {
+    updateProgress();
+  }, 100);
 
   return (
     <Section
@@ -36,23 +58,51 @@ export const FeatureSection: FC<FeatureSectionProps> = (props) => {
         <Title {...featureSectionData} />
 
         <ImageSwitcher
-          imageNodes={featureSectionData.items.map((item) => item.imageNode)}
+          active={currentIndex}
+          featureSectionDataItems={featureSectionData.items}
         />
+
+        <Box
+          xs={{
+            height: 1,
+            background: 'rgba(255, 255, 255, 0.16)',
+            overflow: 'hidden',
+            position: 'relative',
+          }}
+        >
+          <div css={{ width: '100%' }} ref={ref} />
+          <Box
+            xs={{
+              willChange: 'width',
+              transition: 'width 0.2s linear',
+              width:
+                // fixed width
+                progressBarWidth +
+                // dynamic width
+                (progressContainerWidth - progressBarWidth) * progress,
+              height: 1,
+              background: theme.colors.white,
+            }}
+          />
+        </Box>
       </Container>
 
-      <FreeSwiper
-        spaceBetween={24}
-        style={{
-          paddingLeft: 24,
-          paddingRight: 24,
-        }}
-      >
-        {featureSectionData.items.map((item) => (
-          <SwiperSlide style={{ width: 272 }}>
-            <TextBlock key={item.name} {...item} />
-          </SwiperSlide>
-        ))}
-      </FreeSwiper>
+      <Box xs={{ paddingTop: 40 }}>
+        <FreeSwiper
+          onSwiper={setBlockSwiper}
+          spaceBetween={24}
+          style={{
+            paddingLeft: 24,
+            paddingRight: 24,
+          }}
+        >
+          {featureSectionData.items.map((item) => (
+            <SwiperSlide key={item.name} style={{ width: 272 }}>
+              <TextBlock {...item} />
+            </SwiperSlide>
+          ))}
+        </FreeSwiper>
+      </Box>
 
       <Box>{children}</Box>
     </Section>
--- src/components/pages/Security/FeatureSection/ImageSwitcher/ImageSwitcher.tsx
+++ src/components/pages/Security/FeatureSection/ImageSwitcher/ImageSwitcher.tsx
@@ -1,25 +1,31 @@
 import { FC, ReactNode } from 'react';
 
 import { Box } from '../../../../base';
+import { FeatureSectionDataItem } from '../useFeatureSectionData';
 
 export interface ImageSwitcherProps {
   children?: ReactNode;
-  imageNodes: ReactNode[];
+  featureSectionDataItems: FeatureSectionDataItem[];
+  active: number;
 }
 
 export const ImageSwitcher: FC<ImageSwitcherProps> = (props) => {
-  const { imageNodes, children } = props;
+  const { featureSectionDataItems, active = 0, children } = props;
 
   return (
     <Box xs={{ position: 'relative', maxWidth: 1064 }}>
-      {imageNodes.map((node, index) => (
+      {featureSectionDataItems.map((item, index) => (
         <Box
+          key={item.name}
           xs={{
+            opacity: index === active ? 1 : 0,
+            willChange: 'opacity',
+            transition: 'opacity 0.5s',
             top: 0,
             position: index === 0 ? 'relative' : 'absolute',
           }}
         >
-          {node}
+          {item.imageNode}
         </Box>
       ))}
       {children}
