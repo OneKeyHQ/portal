29e3e5f8c2f2030ae195cf91f01cb96c1328ef7e
limichange <limichange@hotmail.com>
2022-06-12 12:00:46 +0800

    refactor: MobileLayout

--- src/components/pages/Security/FeatureSection/FeatureSection.tsx
+++ src/components/pages/Security/FeatureSection/FeatureSection.tsx
@@ -1,14 +1,10 @@
-import { FC, ReactNode, useState } from 'react';
+import { FC, ReactNode } from 'react';
 
 import { useTheme } from '@emotion/react';
-import Swiper from 'swiper';
 
-import { useBoundingClientRect, useInterval } from '../../../../hooks';
-import { Box, Container, Section, SwiperSlide } from '../../../base';
-import { FreeSwiper } from '../../../common';
+import { Box, Container, Section } from '../../../base';
 
-import { ImageSwitcher } from './ImageSwitcher';
-import { TextBlock } from './TextBlock';
+import { MobileLayout } from './MobileLayout';
 import { Title } from './Title';
 import { useFeatureSectionData } from './useFeatureSectionData';
 
@@ -20,26 +16,6 @@ export const FeatureSection: FC<FeatureSectionProps> = (props) => {
   const { children } = props;
   const theme = useTheme();
   const featureSectionData = useFeatureSectionData();
-  const [blockSwiper, setBlockSwiper] = useState<Swiper>();
-  const [progress, setProgress] = useState(0);
-  const progressBarWidth = 100;
-  const { ref, rect } = useBoundingClientRect();
-  const progressContainerWidth = rect?.width ?? 0;
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const updateProgress = () => {
-    if (blockSwiper?.progress) {
-      setCurrentIndex(
-        Math.abs(parseInt((blockSwiper.progress * 2).toFixed(0))),
-      );
-
-      setProgress(blockSwiper?.progress);
-    }
-  };
-
-  useInterval(() => {
-    updateProgress();
-  }, 100);
 
   return (
     <Section
@@ -56,53 +32,9 @@ export const FeatureSection: FC<FeatureSectionProps> = (props) => {
     >
       <Container>
         <Title {...featureSectionData} />
-
-        <ImageSwitcher
-          active={currentIndex}
-          featureSectionDataItems={featureSectionData.items}
-        />
-
-        <Box
-          xs={{
-            height: 1,
-            background: 'rgba(255, 255, 255, 0.16)',
-            overflow: 'hidden',
-            position: 'relative',
-          }}
-        >
-          <div css={{ width: '100%' }} ref={ref} />
-          <Box
-            xs={{
-              willChange: 'width',
-              transition: 'width 0.2s linear',
-              width:
-                // fixed width
-                progressBarWidth +
-                // dynamic width
-                (progressContainerWidth - progressBarWidth) * progress,
-              height: 1,
-              background: theme.colors.white,
-            }}
-          />
-        </Box>
       </Container>
 
-      <Box xs={{ paddingTop: 40 }}>
-        <FreeSwiper
-          onSwiper={setBlockSwiper}
-          spaceBetween={24}
-          style={{
-            paddingLeft: 24,
-            paddingRight: 24,
-          }}
-        >
-          {featureSectionData.items.map((item) => (
-            <SwiperSlide key={item.name} style={{ width: 272 }}>
-              <TextBlock {...item} />
-            </SwiperSlide>
-          ))}
-        </FreeSwiper>
-      </Box>
+      <MobileLayout />
 
       <Box>{children}</Box>
     </Section>
