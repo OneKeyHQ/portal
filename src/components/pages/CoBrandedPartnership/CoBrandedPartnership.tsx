d504b06e82c097ecae400179d24266378964a840
limichange <limichange@hotmail.com>
2022-06-12 22:44:04 +0800

    feat: Hero component content

--- src/components/pages/CoBrandedPartnership/CoBrandedPartnership.tsx
+++ src/components/pages/CoBrandedPartnership/CoBrandedPartnership.tsx
@@ -1,5 +1,12 @@
 import { FC, ReactNode } from 'react';
 
+import { Helmet } from 'react-helmet';
+
+import { Box, Main, Navigation } from '../../base';
+import { PageFooter } from '../../common';
+
+import { Hero } from './Hero';
+
 export interface CoBrandedPartnershipProps {
   children?: ReactNode;
 }
@@ -7,5 +14,20 @@ export interface CoBrandedPartnershipProps {
 export const CoBrandedPartnership: FC<CoBrandedPartnershipProps> = (props) => {
   const { children } = props;
 
-  return <div>CoBrandedPartnership {children}</div>;
+  return (
+    <Box>
+      <Helmet>
+        <title>OneKey</title>
+      </Helmet>
+      <Navigation />
+
+      <Main>
+        <Hero />
+
+        {children}
+      </Main>
+
+      <PageFooter isShowEmailSubscribe={false} isShowMediaLinks={false} />
+    </Box>
+  );
 };
