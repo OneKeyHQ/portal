36260f79909db7f9bd651676382832b8cfa5c3b0
limichange <limichange@hotmail.com>
2022-06-13 09:05:24 +0800

    feat: OurOfferSection responsive style

--- src/components/pages/CoBrandedPartnership/OurOfferSection/OurOfferSection.tsx
+++ src/components/pages/CoBrandedPartnership/OurOfferSection/OurOfferSection.tsx
@@ -1,6 +1,8 @@
 import { FC, ReactNode } from 'react';
 
-import { Box, Container, Flex, H2, Section, Span } from '../../../base';
+import { useTheme } from '@emotion/react';
+
+import { Container, Flex, H2, Section, Span } from '../../../base';
 import { FeatureInformationCard } from '../../../common';
 
 import { useOurOfferSectionData } from './useOurOfferSectionData';
@@ -12,14 +14,36 @@ export interface OurOfferSectionProps {
 export const OurOfferSection: FC<OurOfferSectionProps> = (props) => {
   const { children } = props;
   const ourOfferSectionData = useOurOfferSectionData();
+  const theme = useTheme();
 
   return (
     <Section xs={{ paddingTop: 80, paddingBottom: 80 }}>
-      <Box xs={{ textAlign: 'center' }}>
-        <H2>{ourOfferSectionData.title}</H2>
+      <Flex
+        xs={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          textAlign: 'center',
+          gap: 20,
+        }}
+      >
+        <H2
+          xs={theme.text.medium700}
+          s={theme.text.medium800}
+          m={theme.text.medium900}
+        >
+          {ourOfferSectionData.title}
+        </H2>
 
-        <Span>{ourOfferSectionData.description}</Span>
-      </Box>
+        <Span
+          css={{ maxWidth: 400 }}
+          xs={theme.text.normal300}
+          s={theme.text.normal400}
+          m={theme.text.normal500}
+        >
+          {ourOfferSectionData.description}
+        </Span>
+      </Flex>
 
       <Container>
         <Flex xs={{ gap: 20, paddingTop: 80 }}>
