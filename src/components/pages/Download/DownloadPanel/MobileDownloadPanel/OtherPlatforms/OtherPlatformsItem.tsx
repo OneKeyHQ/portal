d81f6edb1f0bae05ef10c112ecf5cf4e9f31255e
limichange <limichange@hotmail.com>
2022-06-14 22:31:23 +0800

    feat: add download link to list item

--- src/components/pages/Download/DownloadPanel/MobileDownloadPanel/OtherPlatforms/OtherPlatformsItem.tsx
+++ src/components/pages/Download/DownloadPanel/MobileDownloadPanel/OtherPlatforms/OtherPlatformsItem.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode, useState } from 'react';
 
 import { useTheme } from '@emotion/react';
 
-import { Box, Flex, Span } from '../../../../../base';
+import { Box, Flex, Link, Span } from '../../../../../base';
 
 import { Toggle } from './Toggle';
 import { OtherPlatformsDataItem } from './useOtherPlatformsData';
@@ -12,39 +12,48 @@ export interface OtherPlatformsItemProps extends OtherPlatformsDataItem {
 }
 
 export const OtherPlatformsItem: FC<OtherPlatformsItemProps> = (props) => {
-  const { subItems, icon: Icon, children, name, description } = props;
+  const { subItems, icon: Icon, children, name, description, url = '' } = props;
   const theme = useTheme();
   const hasSubItems = subItems && subItems.length > 0;
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <Flex xs={{ flexDirection: 'column' }}>
-      <Flex xs={{ gap: 12, alignItems: 'center' }}>
-        <Icon width={40} height={40} />
-        <Flex xs={{ flexDirection: 'column' }}>
-          <Span xs={theme.text.medium300}>{name}</Span>
+      <Link to={url}>
+        <Flex
+          onClick={() => {
+            setIsExpanded(!isExpanded);
+          }}
+          xs={{
+            gap: 12,
+            alignItems: 'center',
+            color: theme.colors.test500,
+            ':hover': {
+              opacity: 0.6,
+            },
+          }}
+        >
+          <Icon width={40} height={40} />
+          <Flex xs={{ flexDirection: 'column' }}>
+            <Span xs={theme.text.medium300}>{name}</Span>
 
-          {description && (
-            <Span
-              css={{ color: theme.colors.test300 }}
-              xs={theme.text.normal100}
-            >
-              {description}
-            </Span>
+            {description && (
+              <Span
+                css={{ color: theme.colors.test300 }}
+                xs={theme.text.normal100}
+              >
+                {description}
+              </Span>
+            )}
+          </Flex>
+
+          {hasSubItems && (
+            <Flex xs={{ flex: 1, justifyContent: 'flex-end' }}>
+              <Toggle isExpanded={isExpanded} />
+            </Flex>
           )}
         </Flex>
-
-        {hasSubItems && (
-          <Flex
-            onClick={() => {
-              setIsExpanded(!isExpanded);
-            }}
-            xs={{ flex: 1, justifyContent: 'flex-end' }}
-          >
-            <Toggle isExpanded={isExpanded} />
-          </Flex>
-        )}
-      </Flex>
+      </Link>
 
       <Flex
         xs={{
--- src/components/pages/Download/DownloadPanel/MobileDownloadPanel/OtherPlatforms/useOtherPlatformsData.ts
+++ src/components/pages/Download/DownloadPanel/MobileDownloadPanel/OtherPlatforms/useOtherPlatformsData.ts
@@ -17,7 +17,7 @@ export type OtherPlatformsDataItem = {
   icon: React.FC<React.SVGProps<SVGSVGElement>>;
   name: string;
   description: string;
-  link?: string;
+  url?: string;
   subItems?: OtherPlatformsDataItem[];
 };
 
@@ -37,19 +37,19 @@ export function useOtherPlatformsData(): {
             icon: AppleIcon,
             name: 'App Store',
             description: `v${formattedData.ios.version}, for iOS 13.0+`,
-            link: formattedData.ios.url,
+            url: formattedData.ios.url,
           },
           {
             icon: GooglePlayIcon,
             name: 'Google Play',
             description: `v${formattedData.androidGooglePlay.version}, for Android 8.0+`,
-            link: formattedData.androidGooglePlay.url,
+            url: formattedData.androidGooglePlay.url,
           },
           {
             icon: AndroidIcon,
             name: 'Android APK',
             description: `v${formattedData.androidAPK.version}, for Android 8.0+`,
-            link: formattedData.androidAPK.url,
+            url: formattedData.androidAPK.url,
           },
         ],
       },
@@ -62,19 +62,19 @@ export function useOtherPlatformsData(): {
             icon: AppleIcon,
             name: 'macOS',
             description: `v${formattedData.mac.version}, for macOS 10.8+`,
-            link: formattedData.mac.url,
+            url: formattedData.mac.url,
           },
           {
             icon: WindowsIcon,
             name: 'Windows',
             description: `v${formattedData.win.version}, for Windows 10+`,
-            link: formattedData.win.url,
+            url: formattedData.win.url,
           },
           {
             icon: LinuxIcon,
             name: 'Linux',
             description: `v${formattedData.linux.version}, for Ubuntu 12.04+, Fedora 21+, Debian 8+`,
-            link: formattedData.linux.url,
+            url: formattedData.linux.url,
           },
         ],
       },
@@ -87,19 +87,19 @@ export function useOtherPlatformsData(): {
             icon: ChromeIcon,
             name: 'Chrome',
             description: '',
-            link: formattedData.chrome.url,
+            url: formattedData.chrome.url,
           },
           {
             icon: FirefoxIcon,
             name: 'Firefox',
             description: '',
-            link: formattedData.firefox.url,
+            url: formattedData.firefox.url,
           },
           {
             icon: EdgeIcon,
             name: 'Edge',
             description: '',
-            link: formattedData.edge.url,
+            url: formattedData.edge.url,
           },
         ],
       },
