27908c91e927b897b04e7ad1a5fd43ed697fcbe8
limichange <limichange@hotmail.com>
2022-06-14 22:56:55 +0800

    feat: update the Other Contet component layout

--- src/components/pages/Download/DownloadPanel/MobileDownloadPanel/content/OtherContent.tsx
+++ src/components/pages/Download/DownloadPanel/MobileDownloadPanel/content/OtherContent.tsx
@@ -1,5 +1,8 @@
 import { FC, ReactNode } from 'react';
 
+import { Box, Container, Divider, Flex } from '../../../../../base';
+import { Title } from '../../Title';
+
 export interface OtherContentProps {
   children?: ReactNode;
 }
@@ -7,5 +10,22 @@ export interface OtherContentProps {
 export const OtherContent: FC<OtherContentProps> = (props) => {
   const { children } = props;
 
-  return <div>OtherContent {children}</div>;
+  return (
+    <Container>
+      <Flex
+        xs={{
+          height: 656,
+          position: 'relative',
+          zIndex: 100,
+          alignItems: 'flex-end',
+        }}
+      >
+        <Box xs={{ paddingBottom: 40 }}>
+          <Title text={['Bring your crypto assets to mobile, too.']} />
+          <Divider />
+        </Box>
+      </Flex>
+      {children}
+    </Container>
+  );
 };
