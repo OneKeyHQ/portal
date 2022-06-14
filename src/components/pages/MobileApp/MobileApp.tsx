84c03201c610d32cfa130557fecdf36dc98e324c
limichange <limichange@hotmail.com>
2022-06-14 10:15:51 +0800

    feat: create useRuntimeDetect hook

--- src/components/pages/MobileApp/MobileApp.tsx
+++ src/components/pages/MobileApp/MobileApp.tsx
@@ -1,22 +1,21 @@
 import { FC, useEffect, useRef } from 'react';
 
-import { detect } from 'detect-browser';
 import { navigate } from 'gatsby';
 import queryString from 'query-string';
 
 import { useOneKeyVersion } from '../../../data';
+import { useRuntimeDetect } from '../../../hooks';
 import { isBrowser } from '../../../utils';
 import AppPage from '../Download';
 
 export const MobileApp: FC = () => {
   const isDownloading = useRef<boolean>();
+  const runtimeDetect = useRuntimeDetect();
 
   const { formattedData: formattedOneKeyVersionData } = useOneKeyVersion();
 
   useEffect(() => {
     if (isBrowser()) {
-      const browser = detect();
-
       // onekey.so/mobile-app?type=apk
       const parsed = queryString.parse(window.location.search);
 
@@ -25,9 +24,9 @@ export const MobileApp: FC = () => {
           isDownloading.current = true;
           navigate(formattedOneKeyVersionData.androidAPK.url);
         }
-      } else if (browser?.os === 'iOS') {
+      } else if (runtimeDetect.isIOS) {
         navigate(formattedOneKeyVersionData.ios.url);
-      } else if (browser?.os === 'Android OS') {
+      } else if (runtimeDetect.isAndroid) {
         navigate(formattedOneKeyVersionData.androidGooglePlay.url);
       } else {
         navigate('https://onekey.so/download');
@@ -35,7 +34,11 @@ export const MobileApp: FC = () => {
     }
 
     return () => {};
-  }, [formattedOneKeyVersionData]);
+  }, [
+    formattedOneKeyVersionData,
+    runtimeDetect.isAndroid,
+    runtimeDetect.isIOS,
+  ]);
 
   // todo: import download content
   return <AppPage />;
---
+++ src/hooks/useRuntimeDetect.ts
@@ -0,0 +1,10 @@
+import { detect } from 'detect-browser';
+
+export function useRuntimeDetect() {
+  const browser = detect();
+
+  return {
+    isIOS: browser?.os === 'iOS',
+    isAndroid: browser?.os === 'Android OS',
+  };
+}
