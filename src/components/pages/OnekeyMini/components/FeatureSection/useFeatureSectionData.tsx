a8ea1d4fb25b862a7beac7bb2bbf9c96071e2743
limichange <limichange@hotmail.com>
2022-06-16 11:44:15 +0800

    feat: update FeatureSection images

--- src/components/pages/OnekeyMini/components/FeatureSection/useFeatureSectionData.tsx
+++ src/components/pages/OnekeyMini/components/FeatureSection/useFeatureSectionData.tsx
@@ -5,17 +5,17 @@ import { StaticImage } from 'gatsby-plugin-image';
 export function useFeatureSectionData(): FeatureSectionDataItem[] {
   return [
     {
-      imageNode: <StaticImage alt="illustration" src="./images/image.png" />,
+      imageNode: <StaticImage alt="illustration" src="./images/Product1.jpg" />,
       name: 'Open source',
       description: 'Open source Transparent, WYSIWYG',
     },
     {
-      imageNode: <StaticImage alt="illustration" src="./images/image.png" />,
+      imageNode: <StaticImage alt="illustration" src="./images/Product2.jpg" />,
       name: 'Easy to use',
       description: 'Beginner friendly',
     },
     {
-      imageNode: <StaticImage alt="illustration" src="./images/image.png" />,
+      imageNode: <StaticImage alt="illustration" src="./images/Product3.jpg" />,
       name: 'Self-destruction seal',
       description:
         'To ensure every new items on your hand, every step is for safety.',
