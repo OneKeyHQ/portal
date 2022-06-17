1a0481468b2855cc9ed8a51dbfcc568920bf9bd7
limichange <limichange@hotmail.com>
2022-06-17 10:36:55 +0800

    feat: update product price

--- src/components/pages/OnekeyMini/components/RecommendSection/useRecommendSectionData.tsx
+++ src/components/pages/OnekeyMini/components/RecommendSection/useRecommendSectionData.tsx
@@ -8,20 +8,24 @@ export type RecommendSectionDataItem = {
   image: string;
 };
 
+// OneKey Mini $58
+// OneKey Touch $135
+// OneKey Pro $195
+
 export function useRecommendSectionData(): RecommendSectionDataItem[] {
   return [
     {
       name: 'OneKey Touch',
       description:
         'Secure, buy, exchange, grow your crypto and manage your NFTs with our new Bluetooth-enabled hardware wallet.',
-      price: 48,
+      price: 135,
       image: TouchImage,
     },
     {
       name: 'OneKey Pro',
       description:
         'Secure, trade, grow your crypto and manage your NFTs with our new Bluetooth-enabled hardware wallet.',
-      price: 48,
+      price: 195,
       image: ProImage,
     },
   ];
