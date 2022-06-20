b1c32321a4b6997df1d6ba4eb67fc2e7858cf42c
limichange <limichange@hotmail.com>
2022-06-20 11:41:21 +0800

    feat: show BridgeDownload links

--- src/components/pages/Download/BridgeDownload/BridgeDownload.tsx
+++ src/components/pages/Download/BridgeDownload/BridgeDownload.tsx
@@ -1,6 +1,11 @@
 import { FC, ReactNode } from 'react';
 
+import { Menu } from '@headlessui/react';
+
 import { useOneKeyVersion } from '../../../../data';
+import { Box } from '../../../base';
+
+const { Button: MenuButton } = Menu;
 
 export interface BridgeDownloadProps {
   children?: ReactNode;
@@ -8,9 +13,35 @@ export interface BridgeDownloadProps {
 
 export const BridgeDownload: FC<BridgeDownloadProps> = (props) => {
   const { children } = props;
-  const data = useOneKeyVersion();
+  const { formattedData } = useOneKeyVersion();
+  const { bridge } = formattedData;
 
-  console.log('data', data);
+  const options = [
+    bridge.mac,
+    bridge.win,
+    bridge.linux32Deb,
+    bridge.linux64Deb,
+    bridge.linux32Rpm,
+    bridge.linux64Rpm,
+  ];
 
-  return <div>BridgeDownload {children}</div>;
+  return (
+    <div>
+      <Box>
+        <Menu>
+          <MenuButton<'button'>>More</MenuButton>
+          <Menu.Items<'div'>>
+            <Box>{bridge.version}</Box>
+            {options.map((option) => (
+              <Menu.Item<'div'> key={option.url}>
+                <a href={option.url}>{option.name}</a>
+              </Menu.Item>
+            ))}
+          </Menu.Items>
+        </Menu>
+      </Box>
+
+      {children}
+    </div>
+  );
 };
