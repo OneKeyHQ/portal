dec78f5f6b32e3c4930ba8d91f469e5215b11f35
limichange <limichange@hotmail.com>
2022-06-22 18:24:10 +0800

    feat: add GATSBY format env

--- src/utils/staticAssetPrefix.ts
+++ src/utils/staticAssetPrefix.ts
@@ -1,7 +1,8 @@
 import pathBrowserify from 'path-browserify';
 
 export function staticAssetPrefix(path: string): string {
-  const publicUrl = process.env.PUBLIC_URL || '';
+  const publicUrl =
+    process.env.PUBLIC_URL || process.env.GATSBY_PUBLIC_URL || '';
   const newUrl = pathBrowserify.join(publicUrl, path);
 
   return newUrl;
