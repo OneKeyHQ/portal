aaaeca24df3b3c4b1af8726d2941a52bd50275ca
limichange <limichange@hotmail.com>
2022-06-22 17:30:08 +0800

    feat: add demo data

--- src/components/pages/EIPs/useEIPs.ts
+++ src/components/pages/EIPs/useEIPs.ts
@@ -6,9 +6,35 @@ export function useEIPs() {
       description:
         'Ethereum Improvement Proposals (EIPs) describe standards for the Ethereum platform, including core protocol specifications, client APIs, and contract standards. Network upgrades are discussed separately in the Ethereum Project Management repository.',
     },
+    thead: {
+      EIPs: 'EIPs',
+      mobileApp: 'MOBILE APP',
+      hardwareWallet: 'HARDWARE WALLET',
+    },
     items: [
       {
-        name: '',
+        id: 'EIP 721',
+        name: 'ERC-721 non-fungible token standard',
+        suggested: {
+          mobileApp: true,
+          hardwareWallet: true,
+        },
+      },
+      {
+        id: 'EIP 722',
+        name: 'ERC-722 non-fungible token standard',
+        suggested: {
+          mobileApp: true,
+          hardwareWallet: false,
+        },
+      },
+      {
+        id: 'EIP 723',
+        name: 'ERC-723 non-fungible token standard',
+        suggested: {
+          mobileApp: false,
+          hardwareWallet: false,
+        },
       },
     ],
   };
