622b08076022aafd37484f72ab78bba813b89704
limichange <limichange@hotmail.com>
2022-06-22 22:19:11 +0800

    chore: remove log

--- src/utils/staticAssetPrefix.ts
+++ src/utils/staticAssetPrefix.ts
@@ -4,7 +4,5 @@ export function staticAssetPrefix(path: string): string {
   const publicUrl = process.env.PUBLIC_URL || '';
   const newUrl = urlJoin(publicUrl, path);
 
-  console.log(newUrl);
-
   return newUrl;
 }
