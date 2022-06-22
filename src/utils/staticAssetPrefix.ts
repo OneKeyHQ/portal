825eace55e771e338be216f9bda81e1c0801f738
limichange <limichange@hotmail.com>
2022-06-22 20:24:57 +0800

    feat: setting PUBLIC_URL global variable

--- src/utils/staticAssetPrefix.ts
+++ src/utils/staticAssetPrefix.ts
@@ -1,8 +1,7 @@
 import pathBrowserify from 'path-browserify';
 
 export function staticAssetPrefix(path: string): string {
-  const publicUrl =
-    process.env.PUBLIC_URL || process.env.GATSBY_PUBLIC_URL || '';
+  const publicUrl = PUBLIC_URL || '';
   const newUrl = pathBrowserify.join(publicUrl, path);
 
   return newUrl;
