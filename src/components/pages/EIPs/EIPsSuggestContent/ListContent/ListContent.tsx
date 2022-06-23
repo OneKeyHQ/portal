f07fbb372853a51e10578937d0af8d6d96a8055a
limichange <limichange@hotmail.com>
2022-06-23 11:17:40 +0800

    feat: adjust layout

--- src/components/pages/EIPs/EIPsSuggestContent/ListContent/ListContent.tsx
+++ src/components/pages/EIPs/EIPsSuggestContent/ListContent/ListContent.tsx
@@ -1,6 +1,10 @@
 import { FC, ReactNode } from 'react';
 
-import { Box } from '../../../../base';
+import { useTheme } from '@emotion/react';
+
+import { Box, Divider } from '../../../../base';
+import { useEIPs } from '../../useEIPs';
+import { EIPContentItem } from '../EIPContentItem';
 
 export interface ListContentProps {
   children?: ReactNode;
@@ -8,6 +12,40 @@ export interface ListContentProps {
 
 export const ListContent: FC<ListContentProps> = (props) => {
   const { children } = props;
+  const EIPsData = useEIPs();
+  const theme = useTheme();
 
-  return <Box>{children}</Box>;
+  const borderStyle = `1px solid ${theme.colors.test200}`;
+
+  return (
+    <Box
+      xs={{
+        background: theme.colors.white,
+        borderTop: borderStyle,
+        borderBottom: borderStyle,
+
+        paddingTop: 32,
+        paddingBottom: 32,
+      }}
+    >
+      {EIPsData.suggestList.map((item) => (
+        <Box key={item.id}>
+          <Box
+            xs={{
+              paddingLeft: 24,
+              paddingRight: 24,
+            }}
+          >
+            <EIPContentItem {...item} />
+          </Box>
+
+          <Box xs={{ paddingTop: 32, paddingBottom: 32 }}>
+            <Divider color={theme.colors.test200} />
+          </Box>
+        </Box>
+      ))}
+
+      {children}
+    </Box>
+  );
 };
