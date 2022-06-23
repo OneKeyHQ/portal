44f020d096d1f3a2ea4c7e662b9d28dad6206756
limichange <limichange@hotmail.com>
2022-06-23 16:35:19 +0800

    feat: filter list

--- src/components/pages/Cryptos/CryptosTable/suggestList.ts
+++ src/components/pages/Cryptos/CryptosTable/suggestList.ts
@@ -20,16 +20,26 @@ export const touchSuggestList = [
   currencies.HECO,
   currencies.ETH,
   currencies.BCH,
+  currencies.BTG,
 ];
 export const proSuggestList = touchSuggestList;
 
-export const suggestList = Object.values(currencies).map((currency) => ({
-  currency,
-  support: {
-    app: appSuggestList.includes(currency),
-    classic: classicSuggestList.includes(currency),
-    mini: miniSuggestList.includes(currency),
-    touch: touchSuggestList.includes(currency),
-    pro: proSuggestList.includes(currency),
-  },
-}));
+export const suggestList = Object.values(currencies)
+  .map((currency) => ({
+    currency,
+    support: {
+      app: appSuggestList.includes(currency),
+      classic: classicSuggestList.includes(currency),
+      mini: miniSuggestList.includes(currency),
+      touch: touchSuggestList.includes(currency),
+      pro: proSuggestList.includes(currency),
+    },
+  }))
+  .filter(
+    (currency) =>
+      currency.support.app ||
+      currency.support.classic ||
+      currency.support.mini ||
+      currency.support.touch ||
+      currency.support.pro,
+  );
