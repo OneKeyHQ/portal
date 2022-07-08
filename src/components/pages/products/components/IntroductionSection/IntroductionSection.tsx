fbdc9ebb7fa7922b3439ba80dda88bbb318bd30d
limichange <limichange@hotmail.com>
2022-07-08 20:36:50 +0800

    feat: IntroductionSectionTitlet

--- src/components/pages/products/components/IntroductionSection/IntroductionSection.tsx
+++ src/components/pages/products/components/IntroductionSection/IntroductionSection.tsx
@@ -1,10 +1,9 @@
 import { FC, ReactNode } from 'react';
 
-import { useTheme } from '@emotion/react';
-
-import { Box, Container, I18n, Section, Span } from '../../../../base';
+import { Box, Section } from '../../../../base';
 
 import { ImageIntroduction, ImageIntroductionProps } from './ImageIntroduction';
+import { IntroductionSectionTitle } from './IntroductionSectionTitle';
 // import { FullscreenScrollAnimation } from '../FullscreenScrollAnimation';
 
 export interface IntroductionSectionProps extends ImageIntroductionProps {
@@ -14,7 +13,6 @@ export interface IntroductionSectionProps extends ImageIntroductionProps {
 
 export const IntroductionSection: FC<IntroductionSectionProps> = (props) => {
   const { children, items, name } = props;
-  const theme = useTheme();
 
   return (
     <Section>
@@ -22,28 +20,7 @@ export const IntroductionSection: FC<IntroductionSectionProps> = (props) => {
         xs={{ position: 'relative', paddingTop: 72 }}
         xl={{ paddingTop: 120 }}
       >
-        <Container>
-          <Box xs={{ textAlign: 'center' }}>
-            <Span
-              xs={{
-                ...theme.text.medium500,
-                color: theme.colors.test300,
-              }}
-              m={{ ...theme.text.medium600 }}
-            >
-              <I18n name="title__introducing" />
-            </Span>
-            <Box xs={{ paddingTop: 8 }}>
-              <Span
-                xs={theme.text.medium800}
-                m={theme.text.medium900}
-                xl={theme.text.medium1000}
-              >
-                {name}
-              </Span>
-            </Box>
-          </Box>
-        </Container>
+        <IntroductionSectionTitle name={name} />
 
         <Box
           xs={{ paddingTop: 40, paddingLeft: 24, paddingRight: 24 }}
