56fcd9470fb49d10456a1a5460046a67fd648516
limichange <limichange@hotmail.com>
2022-07-19 14:23:07 +0800

    feat: create SectionTitle component

---
+++ src/components/common/SectionTitle/SectionTitle.tsx
@@ -0,0 +1,29 @@
+import { FC, ReactNode } from 'react';
+
+import { useTheme } from '@emotion/react';
+
+import { Span } from '../../base';
+
+export interface SectionTitleProps {
+  children?: ReactNode;
+}
+
+export const SectionTitle: FC<SectionTitleProps> = (props) => {
+  const { children } = props;
+  const theme = useTheme();
+
+  return (
+    <Span
+      xs={{
+        ...theme.text.medium700,
+        color: theme.colors.test500,
+      }}
+      m={theme.text.medium800}
+      l={theme.text.medium900}
+      xl={theme.text.medium1000}
+      xxl={theme.text.medium1100}
+    >
+      {children}
+    </Span>
+  );
+};
